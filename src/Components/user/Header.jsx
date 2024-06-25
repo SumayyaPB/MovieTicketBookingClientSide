91056636fae875c38b4c5608db14a176cebd5df4
Sumayya PB <sumayyapb38@gmail.com>
2024-06-25 20:42:16 +0530

    initial commit

--- src/Components/user/Header.jsx
+++ src/Components/user/Header.jsx
@@ -1,9 +1,10 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from "react";
 import "./Header.css";
 import userIcon from "../../assets/userIcon.svg";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import movieLogo from '../../assets/MovieLogo (2).png';
+import movieLogo from '../../assets/MovieLogo (2).png'
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,7 +14,7 @@ const Header = () => {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
-
+   
   const handleLogout = async () => {
     try {
       const response = await axios.get("https://movie-ticket-bookingapplication-1.onrender.com/api/v1/user/logout", {
@@ -26,11 +27,9 @@ const Header = () => {
       if (response.status === 200) {
         setIsLoggedIn(false);
         navigate("/user/login");
-      } else {
-        console.log("Unexpected response:", response);
       }
     } catch (error) {
-      console.error("Logout error:", error);
+      console.log(error);
       setIsLoggedIn(false);
       navigate("/user/signin");
     }
@@ -47,7 +46,7 @@ const Header = () => {
       console.log(response.data);
       setIsLoggedIn(response.data.ok);
     } catch (error) {
-      console.error("Login status check error:", error);
+      console.log(error);
       setIsLoggedIn(false);
     }
   };
@@ -60,34 +59,29 @@ const Header = () => {
     <header>
       <nav className="navbar">
         <div className="logo">
-          <Link to="/">
-            <img src={movieLogo} alt="" className="movieLogo" />
-            MOVIE
-          </Link>
+          <Link to="/"><img src={movieLogo} alt="" className="movieLogo"/>MOVIE</Link>
         </div>
         <div className={`nav-links ${isOpen ? "active" : ""}`}>
           <ul>
             <li>
               <Link to="/">Home</Link>
             </li>
-            {isLoggedIn ? (
-              <li className="logout-user">
-                <button onClick={handleLogout} className="logout-button">
-                  Logout
-                </button>
-              </li>
-            ) : (
-              <li className="login-user">
-                <Link to="/user/login">Login</Link>
-              </li>
-            )}
-            <li>
-              <span className="user-icon">
-                <Link to="/userDashboard">
-                  <img src={userIcon} alt="User Dashboard" className="text-light" />
-                </Link>
-              </span>
+          
+            <li className="logout-user">
+                  <button onClick={handleLogout} className="logout-button">Logout</button>
             </li>
+            <li className="login-user">
+                <Link to="/user/login">Login</Link>
+            </li>
+            <li>
+                <span className="user-icon">
+                  <Link to="/userDashboard">
+                      <img src={userIcon} alt="User Dashboard" className="text-light"/>
+                  </Link>
+                </span>
+            </li>
+
+            
           </ul>
         </div>
         <div className="burger-menu" onClick={toggleMenu}>
