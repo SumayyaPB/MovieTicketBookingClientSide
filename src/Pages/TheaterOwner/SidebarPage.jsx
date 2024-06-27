fcc38965853d2aa872371da1f0273516db386a01
Sumayya PB <sumayyapb38@gmail.com>
2024-06-27 23:18:42 +0530

    latest changes

--- src/Pages/TheaterOwner/SidebarPage.jsx
+++ src/Pages/TheaterOwner/SidebarPage.jsx
@@ -1,12 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import Header from "../../Components/TheaterOwner/Sidebar";
+import Sidebar from "../../Components/TheaterOwner/Sidebar";
 import { Outlet } from "react-router-dom";
+import Header from "../../Components/user/Header";
 
 const SidebarPage = () => {
   return (
     <div className="row">
-      <Header />
+      <Header/>
+      <Sidebar/>
       <Outlet />
     </div>
   );
