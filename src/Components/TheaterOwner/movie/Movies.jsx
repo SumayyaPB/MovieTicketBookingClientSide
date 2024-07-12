91f70fb82c386d0a3071b8a3d696e0cd58a52ae7
Sumayya PB <sumayyapb38@gmail.com>
2024-07-12 20:19:40 +0530

    latest commit

--- src/Components/TheaterOwner/movie/Movies.jsx
+++ src/Components/TheaterOwner/movie/Movies.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
-import deleteIcon from "../../../assets/deleteIcon.svg"
+import deleteIcon from "../../../assets/deleteIcon.svg";
+import { BsFillStarFill } from "react-icons/bs";
+import './Movies.css'
 
 const Movies = () => {
   const { setValue, watch } = useForm();
@@ -9,7 +11,9 @@ const Movies = () => {
 
   const getMovies = async () => {
     try {
-      const res = await axios.get("https://movie-ticket-bookingapplication-1.onrender.com/api/v1/movie/getmovies");
+      const res = await axios.get(
+        "https://movie-ticket-bookingapplication-1.onrender.com/api/v1/movie/getmovies"
+      );
       setMovies(res.data);
       console.log("Movies fetched:", res.data);
     } catch (error) {
@@ -19,9 +23,11 @@ const Movies = () => {
 
   const deleteMovie = async (movieId) => {
     try {
-      await axios.delete(`https://movie-ticket-bookingapplication-1.onrender.com/api/v1/movie/deletemovie/${movieId}`);
+      await axios.delete(
+        `https://movie-ticket-bookingapplication-1.onrender.com/api/v1/movie/deletemovie/${movieId}`
+      );
       // Remove the deleted movie from the state
-      setMovies(movies.filter(movie => movie._id !== movieId));
+      setMovies(movies.filter((movie) => movie._id !== movieId));
       console.log(`Movie with ID: ${movieId} deleted successfully`);
     } catch (error) {
       console.error("Error deleting movie:", error);
@@ -33,28 +39,37 @@ const Movies = () => {
   }, []);
 
   return (
-    <div className="items">
+    <div className="movie-items">
       <h1>Movies</h1>
       <div className="d-flex flex-wrap gap-3">
         {movies.length > 0 ? (
           movies.map((movie, index) => (
             <div
-              className={watch("movieId") === movie._id ? "item selected" : "item"}
+              className={
+                watch("movieId") === movie._id ? "item selected" : "item"
+              }
               key={index}
               onClick={() => setValue("movieId", movie._id)}
             >
               <img src={movie.movieImg} alt="" />
               <p>{movie.title}</p>
-              <p>{movie.rating}</p>
-              <p>{Array.isArray(movie.genre) ? movie.genre.join(", ") : movie.genre}</p>
-              <button onClick={(e) => {
+              <p className="rating">
+                <BsFillStarFill className="star" />
+                &nbsp;&nbsp;
+                {movie.rating}/10
+              </p>
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  deleteMovie(movie._id);
+                }}
+              >
+                delete <img src={deleteIcon} className="dlticon"/>
+              </button>
+              {/* <img src={deleteIcon} alt="" onClick={(e) => {
                 e.stopPropagation();
                 deleteMovie(movie._id);
-              }}>delete</button>
-              <img src={deleteIcon} alt="" onClick={(e) => {
-                e.stopPropagation();
-                deleteMovie(movie._id);
-              }}/>
+              }}/> */}
             </div>
           ))
         ) : (
